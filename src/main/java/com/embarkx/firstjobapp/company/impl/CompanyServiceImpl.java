cbefd1c8e573a5051823f3755470649e9c79d23d
Rajeswarrddy <rajeswarreddy8185@gmail.com>
2026-09-20 16:22:56 -0600

    adding comapny service impl and application properties

--- src/main/java/com/embarkx/firstjobapp/company/impl/CompanyServiceImpl.java
+++ src/main/java/com/embarkx/firstjobapp/company/impl/CompanyServiceImpl.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 public class CompanyServiceImpl implements CompanyService {
 
     private CompanyRepository companyRepository;
-
+    
     public CompanyServiceImpl(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
